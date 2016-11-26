b844d485801b5ed34924a8c51fc9f6e1dda266d4
John Dixon <jpdixon@gmail.com>
2016-11-26 12:18:36 -0500

    Add files via upload

---
+++ TestMinder.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+
+import urllib2
+import json
+import time
+import boto3
+
+# TestMinder, v1.0
+
+def getTestSchedule():
+    print("** getTestSchedule")
+
+
+
